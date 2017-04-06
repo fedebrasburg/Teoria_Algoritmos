d5033ab2239350c39cd8a436f674d95bd104b0a0
Pipi <p.ciruzzi@hotmail.com>
2017-04-06 00:56:28 -0300

    Limpieza de codigo innecesario + adaptacion de codigo a las necesidades + comienzo de parser y escritor de archivos necesarios

--- src/grafo.py
+++ src/grafo.py
@@ -1,19 +1,6 @@
 INFINITO = float("inf") # Truco magico de python none <any int, <any string
 
-# TODO: Esta clase no se usa -> Borrar!
-class Nodo(object):
-    def __init__(self, ID, distancia, padre=None):
-        self.nombre = ID
-        self.distancia = distancia
-        self.padre = padre # TODO: Esto no es medio turbio? jajaj
-    def __cmp__(self, otro):
-        if(self.distancia == otro.distancia):
-            return 0
-        if(self.distancia > otro.distancia):
-            return 1
-        return -1
-
-# Yo a la clase Arista tal vez haría que tenga src y dst, y después en el grafo la manejo como una lista de aristas en vez de como un hash
+# Yo a la clase Arista tal vez haria que tenga src y dst, y despues en el grafo la manejo como una lista de aristas en vez de como un hash
 class Arista(object):
     def __init__(self, peso):
         self.peso = peso
@@ -21,57 +8,28 @@ class Arista(object):
         return self.peso
 
 class Grafo(object):
-    def __init__(self):
+    def __init__(self, cantVertices):
         """Crea un Grafo dirigido (o no) con aristas pesadas (o no)"""
         self.aristas = {}
-        self.nodos = {} # TODO: Yo guardaría solo las aristas, y de última, cuántos vértices hay, si total son números
-        self.nombres = {} # TODO: Tiene sentido esto?
+        self.cantVertices = cantVertices
+        for i in range(0, cantVertices):
+            self.aristas[i] = {}
+            a = self.aristas[i]
 
-    def devolver_nodos(self):
+    def devolver_cant_nodos(self):
         """Devuelve los nodos del grafo"""
-        return self.nodos.keys()
+        return self.cantVertices
 
-    def existe_nodo(self, nombre):
+    def existe_nodo(self, ID):
         """Devuelve True si existe el nodo"""
-        return (nombre in self.nodos.keys())
+        return (0 <= ID and ID < self.cantVertices)
 
-    # TODO: En estos 3 métodos de borrar, el raise no iría en un else, ya que así lanza la execpción siempre? Pregunto, no sé cómo lo maneja Python jajaj
-    #       De todas formas, en el enunciado dicen que no se pueden borrar asique, pueden volar los 3 métodos
-    def borrar_vertice(self, ID):
-        """Borra un vertice que se identifica con ID (si tiene aristas asociadas levanta ValueError)"""
-        # TODO: Sino hacer que elimine todas las aristas y después el nodo?
-        if (self.nombres.has_key(ID) and len(self.aristas[ID]) == 0):
-            self.nodos.pop(self.nombres[ID])
-            self.nombres.pop(ID)
-            self.aristas.pop(ID)
-        raise ValueError
-
-    def borrar_arista_no_dirigida(self, id1, id2):
-        """Borra una arista entre id1 y id2 (si no existe devuelve error)"""
-        if (aristas[id1].has_key(id2)):
-            self.aristas[id1].remove(id2)
-        elif (aristas[id2].has_key(id1)): # TODO: No debería ser un if directamente, en vez de elif? Porque eliminaría una sola de las 2 así
-            self.aristas[id2].remove(id1)
-        raise ValueError
-
-    def borrar_arista_dirigida(self, id1, id2):
-        """Borra una arista entre id1 y id2 (si no existe devuelve error)"""
-        if(aristas[id1].has_key(id2)):
-            self.aristas[id1].remove(id2)
-        raise ValueError
-
-    def agregar_vertice(self, nombre, ID):
-        """Agrega un vertice que se identifica con un nombre y un ID"""
-        self.nodos[nombre] = ID
-        self.nombres[ID] = nombre
-        self.aristas[ID] = {}
-
-    def agregar_arista_no_dirigida(self, id1, id2, peso=0):
+    def agregar_arista_no_dirigida(self, id1, id2, peso = 0):
         """Agrego una arista no dirigida entre los nodos con id1 y id2"""
         self.aristas[id1][id2] = Arista(peso)
         self.aristas[id2][id1] = Arista(peso)
 
-    def agregar_arista_dirigida(self, id1, id2, peso=0):
+    def agregar_arista_dirigida(self, id1, id2, peso = 0):
         """Agrego una arista dirigida entre los nodos con id1 y id2"""
         self.aristas[id1][id2] = Arista(peso)
 
@@ -80,6 +38,7 @@ class Grafo(object):
         return (id2 in self.aristas[id1].keys())#Azucar sintactico
 
     def peso_arista(self, id1, id2):
+        """Devuelve el peso de la arista entre id1 e id2"""
         if(self.son_vecinos(id1,id2)):
             return self.aristas[id1][id2].peso
         raise ValueError
@@ -88,17 +47,16 @@ class Grafo(object):
         """Realiza un recorrido DFS"""
         visitado[actual] = True
         lista.append(actual)
-        for ID_ady in self.aristas[self.nodos[actual]].keys():
-            ady = self.nombres[ID_ady]
-            if(visitado[ady] == False):
-                padre[ady] = actual
-                self._DFS_Visitar(ady, visitado, padre, lista)
+        for ID_ady in self.aristas[actual].keys():
+            if(visitado[ID_ady] == False):
+                padre[ID_ady] = actual
+                self._DFS_Visitar(ID_ady, visitado, padre, lista)
 
-    def DFS(self, nombre):
-        """Hace un recorrido DFS desde un nombre """
+    def DFS(self, ID):
+        """Hace un recorrido DFS desde un ID"""
         visitado,padre,distancia = self._inicializar_iterador()
         lista = []
-        self._DFS_Visitar(nombre, visitado, padre, lista)
+        self._DFS_Visitar(ID, visitado, padre, lista)
         return lista
 
     def _inicializar_iterador(self):
@@ -106,7 +64,7 @@ class Grafo(object):
         visitado = {}
         distancia = {}
         padre = {}
-        for actual in self.nodos.keys():
+        for actual in range(0, cantVertices):
             visitado[actual] = False
             distancia[actual] = INFINITO
             padre[actual] = None
--- src/parser.py
+++ src/parser.py
@@ -1,30 +1,53 @@
+from grafo import Grafo
+
 CERO = 0
-PRIMERO = 1
-SEGUNDO = 2
+UNO = 1
 
 class Parser(object):
 
-    def __init__(self):
+    def escribirStableMatching(self, nombre, E, H, Q):
+        """Escribe un archivo del tipo Stable Matching"""
+        try:
+            miArch = open(nombre, 'w')
+            n = len(E)
+            miArch.write(str(n) + '\n')
+            for i in range(0, n):
+                numeros = " ".join(str(x) for x in E[i]) + '\n' # Deberia haber m numeros
+                miArch.write(numeros)
+            m = len(H)
+            miArch.write(str(m) + '\n')
+            for i in range(0, m):
+                numeros = " ".join(str(x) for x in H[i]) + '\n' # Deberia haber n numeros
+                miArch.write(numeros)
+            numeros = " ".join(str(x) for x in Q) + '\n' # Deberia haber m numeros
+            miArch.write(numeros)
+            miArch.close()
+            return True
+        except:
+            print "Ocurrio un error leyendo el archivo"
+            return False
 
-    def leer(self, nombre):
-        """Lee un archivo con nombre"""
-        grafo = Grafo()
+    def leerGrafoNoDirigido(self, nombre):
+        """Lee un archivo de un grafo dirigido"""
+        # idem dirigido, cambiando el metodo a llamar del grafo
+
+    def leerGrafoDirigido(self, nombre):
+        """Lee un archivo de un grafo dirigido sin peso"""
         try:
             miArch = open(nombre)
             cant_nodos = int(miArch.readline())
+            print(cant_nodos)
+            grafo = Grafo(cant_nodos)
             cant_aristas = int(miArch.readline())
-            for i in range(0, cant_nodos):
-                # linea = miArch.readline()
-                # numeros = linea.split(" ")
-                numeros[PRIMERO] = numeros[PRIMERO].rstrip('\n')
-                self.agregar_vertice(numeros[PRIMERO],numeros[CERO])
+            print(cant_aristas)
             for i in range(0, cant_aristas):
                 linea = miArch.readline()
-                numeros = linea.split(" ")
-                numeros[SEGUNDO] = numeros[SEGUNDO].rstrip('\n')
-                self.agregar_arista(numeros[PRIMERO],numeros[SEGUNDO])
+                linea = linea.rstrip('\n')
+                numeros = map(int, linea.split(" "))
+                print(str(numeros[CERO]) + " " + str(numeros[UNO]))
+                grafo.agregar_arista_dirigida(numeros[CERO], numeros[UNO])
             miArch.close()
-            return True
+            return grafo
         except:
             print "Ocurrio un error leyendo el archivo"
             return False
