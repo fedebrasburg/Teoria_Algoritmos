009103404e921d40f9782ac81eefc5b45bfba411
Pipi <p.ciruzzi@hotmail.com>
2017-04-22 13:56:33 -0300

    DFS no iterativo como clase

--- src/CFC.py
+++ src/CFC.py
@@ -1,4 +1,3 @@
-from grafo import Grafo
 from parser import Parser
 from grafo import trasponer
 from dfs import DFS
@@ -7,13 +6,14 @@ import time
 
 def CFC(g):
 	"""Recibe un grafo y devuelve las componentes fuertemente conexas"""
-	r = DFS(g)
-	return DFS(trasponer(g),sorted(r.get_tiempo_finalizado(), key=r.get_tiempo_finalizado().get, reverse = True)).get_bosque_DFS()
+	r = DFS(g).hacer_DFS()
+	return DFS(trasponer(g),sorted(r.get_tiempo_finalizado(), key=r.get_tiempo_finalizado().get, reverse = True)).hacer_DFS().get_bosque_DFS()
 
-for i in [0]:
+
+for i in [0, 1, 2, 3, 4, 5, 6]:
 	start = time.time()
 	parser = Parser()
 	g = parser.leer_grafo_dirigido("../in/ej3/d" + str(i) + ".txt")
-	print CFC(g)
+	CFC(g)
 	end = time.time()
 	print("Con " + str(g.devolver_cant_vertices()) + " vertices y " + str(len(g.devolver_aristas())) + " aristas, tardo: " + str(end - start) + " segundos")
--- src/dfs.py
+++ src/dfs.py
@@ -2,54 +2,58 @@ from tiempo import Tiempo
 from resultado_DFS import ResultadoDFS
 
 
-def DFS(g, lista_vertices = {}):
-	if (lista_vertices == {}):
-		lista_vertices = g.devolver_vertices()
-	visitado = {}
-	tiempo_visitado = {}
-	tiempo = Tiempo()
-	bosque = []
-	tiempo_finalizado = {}
-	predecesor = {}
-	bajo = {}
-	puntos_artic = set()
-	raices = []
-	for v in lista_vertices:
-		visitado[v] = False
-		predecesor[v] = None
-	for v in lista_vertices:
-		if (not visitado[v]):
-			arbol = []
-			DFS_Visitar(g, v, visitado, tiempo, tiempo_visitado, tiempo_finalizado, arbol, bajo, predecesor, puntos_artic)
-			# Como v es raiz del arbol, lo saco para analizarlo por separado
-			if (v in puntos_artic): # Aunque deberia estar incluida siempre
-				puntos_artic.remove(v)
-			raices.append(v)
-			bosque.append(arbol)
-	print "# raices:", len(raices) # Si es conexo deberia ser 1 siempre
-	for v in raices:
-		hijos = 0
-		for u in g.adyacentes(v): # Basta con revisar si los adyacentes a v lo tienen como predecesor o no, no es necesario en todos los vertices
-			if (predecesor[u] == v):
-				hijos += 1
-		if (hijos >= 2):
-			puntos_artic.add(v)
-	return ResultadoDFS(tiempo_visitado, tiempo_finalizado, bosque, puntos_artic)
+class DFS:
 
-def DFS_Visitar(g, v, visitado, tiempo, tiempo_visitado, tiempo_finalizado, arbol, bajo, predecesor, puntos_artic):
-	visitado[v] = True
-	arbol.append(v)
-	tiempo.incrementar()
-	tiempo_visitado[v] = tiempo.actual()
-	bajo[v] = tiempo_visitado[v]
-	for u in g.adyacentes(v):
-		if (not visitado[u]):
-			predecesor[u] = v
-			DFS_Visitar(g, u, visitado, tiempo, tiempo_visitado, tiempo_finalizado, arbol, bajo, predecesor, puntos_artic)
-			bajo[v] = min(bajo[v], bajo[u])
-			if (bajo[u] >= tiempo_visitado[v]):
-				puntos_artic.add(v)
-		elif (u != predecesor[v]): # Arista de retroceso
-			bajo[v] = min(bajo[v], tiempo_visitado[u])
-	tiempo.incrementar()
-	tiempo_finalizado[v] = tiempo.actual()
+	def __init__(self, g, lista_vertices = {}):
+		self.g = g
+		self.lista_vertices = g.devolver_vertices() if (lista_vertices == {}) else lista_vertices
+		self.visitado = {}
+		self.tiempo_visitado = {}
+		self.tiempo = Tiempo()
+		self.bosque = []
+		self.tiempo_finalizado = {}
+		self.predecesor = {}
+		self.bajo = {}
+		self.puntos_artic = set()
+		self.raices = []
+		for v in self.lista_vertices:
+			self.visitado[v] = False
+			self.predecesor[v] = None
+
+	def hacer_DFS(self):
+		for v in self.lista_vertices:
+			if not self.visitado[v]:
+				arbol = []
+				self._DFS_Visitar(v, arbol)
+				# Como v es raiz del arbol, lo saco para analizarlo por separado
+				if v in self.puntos_artic:  # Aunque deberia estar incluida siempre
+					self.puntos_artic.remove(v)
+				self.raices.append(v)
+				self.bosque.append(arbol)
+		print "# raices:", len(self.raices)  # Si es conexo deberia ser 1 siempre
+		for v in self.raices:
+			hijos = 0
+			for u in self.g.adyacentes(v):  # Basta con revisar si los adyacentes a v lo tienen como predecesor o no, no es necesario en todos los vertices
+				if self.predecesor[u] == v:
+					hijos += 1
+			if hijos >= 2:
+				self.puntos_artic.add(v)
+		return ResultadoDFS(self.tiempo_visitado, self.tiempo_finalizado, self.bosque, self.puntos_artic)
+
+	def _DFS_Visitar(self, v, arbol):
+		self.visitado[v] = True
+		arbol.append(v)
+		self.tiempo.incrementar()
+		self.tiempo_visitado[v] = self.tiempo.actual()
+		self.bajo[v] = self.tiempo_visitado[v]
+		for u in self.g.adyacentes(v):
+			if not self.visitado[u]:
+				self.predecesor[u] = v
+				self._DFS_Visitar(u, arbol)
+				self.bajo[v] = min(self.bajo[v], self.bajo[u])
+				if self.bajo[u] >= self.tiempo_visitado[v]:
+					self.puntos_artic.add(v)
+			elif u != self.predecesor[v]:  # Arista de retroceso
+				self.bajo[v] = min(self.bajo[v], self.tiempo_visitado[u])
+		self.tiempo.incrementar()
+		self.tiempo_finalizado[v] = self.tiempo.actual()
