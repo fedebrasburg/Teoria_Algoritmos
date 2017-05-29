3a2bcf074ff9c9c0862387ebbfcfd4023b0a6358
Pipi <p.ciruzzi@hotmail.com>
2017-05-29 00:52:20 -0300

    Cambio tests (y FW) para que usen vertices empezando en indice 0

--- TP2/src/floyd_warshall.py
+++ TP2/src/floyd_warshall.py
@@ -9,19 +9,15 @@ class FloydWarshall(object):
         for i in range(n):
             self.camino[i][i] = 0
 
-    # Recordar que los ids de vertices comienzan en 1, pero en la matriz es en 0, por eso se resta 1
     def floydWarshall(self):
         aristas = self.grafo.devolver_aristas_list()
         for a in aristas:
-            self.camino[a.id1 - 1][a.id2 - 1] = a.peso
+            self.camino[a.id1][a.id2] = a.peso
 
         vertices = self.grafo.devolver_vertices()
         for k in vertices:
-            k = k - 1
             for j in vertices:
-                j = j - 1
                 for i in vertices:
-                    i = i - 1
                     nuevo_camino = self.camino[i][k] + self.camino[k][j]
                     if self.camino[i][j] > nuevo_camino:
                         self.camino[i][j] = nuevo_camino
