0a91ceacaee9ed954a2bd832143d6143182a8b44
Pipi <p.ciruzzi@hotmail.com>
2017-06-20 13:20:46 -0300

    Saco prints comentados

--- TP3/src/subset_sum.py
+++ TP3/src/subset_sum.py
@@ -19,18 +19,14 @@ class SubsetSum(object):
     def recortar_lista(self, listaNumeros, d):
         if not listaNumeros:
             return
-        # print "\nrecortar lista con d",d
         listaNumeros = sorted(listaNumeros)
         n = len(listaNumeros)
         last = listaNumeros[0]
         nuevaListaNumeros = [listaNumeros[0]]
-        # print "ultimo",last
         for i in range(1, n):
-            # print "comparacion",listaNumeros[i],last*(1+d)
             if listaNumeros[i] > (last * (1 + d)):
                 nuevaListaNumeros.append(listaNumeros[i])
                 last = listaNumeros[i]
-        # print "\n"
         return nuevaListaNumeros
 
     def resolverProblema(self, nombre_archivo, t, e):
@@ -41,15 +37,10 @@ class SubsetSum(object):
         L = range(0, n + 1)
         L[0] = [0]
         for i in range(1, n):
-            # print "paso",i
-            # print listaNumeros[i-1]
             nuevalista = map(lambda x: x + listaNumeros[i - 1], L[i - 1])
             L[i] = list(set(L[i - 1] + nuevalista))
-            # print "lista",L[i]
             L[i] = self.recortar_lista(L[i], e / (2 * n))
-            # print "lista recortada",L[i]
             L[i] = [j for j in L[i] if j <= t]
-        # print "lista filtrada",L[i]
         L[n] = sorted(L[n - 1])
         z = L[-1][-1]
         return z
